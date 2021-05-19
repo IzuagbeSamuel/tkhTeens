a54a3414ddaccbfd6ae5e44b8a802d54b49a1cf0
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-05-19 01:19:46 +0100

    Add form, icons to social media handle

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,23 @@ $(document).ready(function(){
 
     event.preventDefault();
   })
+
+  myButton = document.getElementById("myBtn");
+
+  window.onscroll = function() {
+    scrollFunction()
+  };
+
+  window.onclick = function() {
+    topFunction();
+  }
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop
+      > 20) {
+        myButton.style.display = "block";
+      } else {
+        myButton.style.display = "none";
+      }
+  }
 })
 
