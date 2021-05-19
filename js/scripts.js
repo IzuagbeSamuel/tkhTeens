4a9d33a7e6b52967ac696ed41412cfe3607ec9f1
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-05-19 01:43:04 +0100

    Add youtube and mail link

--- js/scripts.js
+++ js/scripts.js
@@ -20,8 +20,8 @@ $(document).ready(function(){
     topFunction();
   }
   function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop
-      > 20) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop
+      > 10) {
         myButton.style.display = "block";
       } else {
         myButton.style.display = "none";
