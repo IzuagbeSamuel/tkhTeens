b66bb41fd28976825dfa7410addcb704d1765a08
Izuagbe Samuel <izuagbesamuel73@gmail.com>
2021-05-23 12:47:12 +0100

    Added form to our teens email

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,4 @@
-$(document).ready(function(){
-  $("#form").submit(function(event){
-    let name = $("#name").val();
 
-    $(".name").append(name);
-
-    $("#hide").show();
-    $("#form").hide();
-
-    event.preventDefault();
-  })
-
-  myButton = document.getElementById("myBtn");
-
-  window.onscroll = function() {
-    scrollFunction()
-  };
-
-  
   function scrollFunction() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop
       > 40) {
@@ -25,5 +7,4 @@ $(document).ready(function(){
         myButton.style.display = "none";
       }
   }
-})
 
